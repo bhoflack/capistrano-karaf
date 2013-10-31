1dbde4c6425e77ffa00c835a51e9c7ea6d46d2de
Brecht Hoflack <brh@melexis.com>
2013-10-31 12:03:57 +0100

    Refactor for capistrano 3.0

--- capistrano-karaf.gemspec
+++ capistrano-karaf.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name          = 'capistrano-karaf'
-  s.version       = '1.0.0'
-  s.date          = '2013-10-04'
+  s.version       = '1.1.0'
+  s.date          = '2013-10-31'
   s.summary       = 'Capistrano functions for communicating with karaf'
   s.authors       = ['Brecht Hoflack']
   s.email         = 'brecht.hoflack@gmail.com'
--- lib/capistrano-karaf.rb
+++ lib/capistrano-karaf.rb
@@ -1,82 +1,101 @@
-require 'capistrano'
+require 'sshkit'
 
-def with_karaf (params={}, &block)
-  params = { :username => 'smx',
-             :password => 'smx',
-             :port     => 8101 }.merge(params)
+SSHKit.config.command_map = 
+    {
+        :features_addurl    => 'features:addurl',
+        :features_removeurl => 'features:removeurl',
+        :features_install   => 'features:install',
+        :features_list      => 'features:list',
+        :features_info      => 'features:info',
+        :headers            => 'headers',
+        :list               => 'list'
+    }
 
-  unless self[:username] == params[:username]
-    set :port, params[:port]
-    set :user, params[:username]
-    set :password, params[:password]
-    close_sessions
-  end
-
-  yield
+def add_url (url)
+    execute(:features_addurl, url)
 end
 
-def close_sessions
-  sessions.values.each {|session| session.close}
-  sessions.clear
+def remove_url (url)
+    execute(:features_removeurl, url)
 end
 
-
-def add_url (url, params={})
-  with_karaf params do
-    run "features:addurl #{url}", {:shell => false}
-  end
+def feature_install (name)
+    execute(:features_install, name)
 end
 
-def remove_url (url, params={})
-  with_karaf params do
-    run "features:removeurl #{url}", {:shell => false}
-  end
+def feature_uninstall (name)
+    execute(:features_uninstall, name)
 end
 
-def feature_install (name, params={})
-  with_karaf params do
-    run "features:install #{name}", {:shell => false, :pty => true}
-  end
-end
-
-def feature_uninstall (name, params={})
-  with_karaf params do
-    run "features:uninstall #{name}", {:shell => false}
-  end
-end
-
-def list_bundles (params = {})
+def list_bundles ()
   bundle_line_matcher = /^\[(?<BundleId>[ \d]+)\] \[(?<BundleStatus>[ \w]+)\] \[[ ]*\] \[(?<ContextStatus>[ \w]+)\] \[(?<BundleLevel> [ \d]+)\] (?<BundleName>[\w\-\:]+) \((?<BundleVersion>.+)\)/
 
-  with_karaf params do
-    run "list" do |_, _, data|
-      bundles = []
-      data.lines.each do |line|
-        m = bundle_line_matcher.match(line)
-        if m then
-          bundles.push({ :id => m['BundleId'],
-                         :status => m['BundleStatus'],
-                         :context => m['ContextStatus'],
-                         :level => m['BundleLevel'],
-                         :name => m['BundleName'],
-                         :version => m['BundleVersion']
-                       })
-        end
+  
+    data = capture(:list)
+    bundles = []
+    data.lines.each do |line|
+      m = bundle_line_matcher.match(line)
+      if m then
+        bundles.push({ :id => m['BundleId'],
+                       :status => m['BundleStatus'],
+                       :context => m['ContextStatus'],
+                       :level => m['BundleLevel'],
+                       :name => m['BundleName'],
+                       :version => m['BundleVersion']
+                     })
       end
       bundles
     end
-  end
 end
 
-def started? (name, params={})
+def started? (name)
   bundle = list_bundles.find {|b| b[:name] == name}
   bundle[0][:context] == 'Started'
 end
 
-def list (params={})
-  with_karaf params do
-    run "features:list", {:shell => false}
-  end
+def list ()
+    capture(:features_list)
 end
 
+def headers (bundle)
+    data = capture(:headers, bundle)
+    extract_bundle_headers(data)
+end
 
+def feature_bundles (name)
+    data = capture(:features_info, name)
+    extract_bundles_from_feature(data)
+end
+
+def feature_bundles_numbers (name)
+  bundles = list_bundles
+  bundleIds = bundles.map {|bundle| bundle[:id]}
+  bundleHeaders = bundleIds.map {|bundleId| headers(bundleId).store(:bundleId, bundleId)}
+  featureBundles = feature_bundles(name)
+  bundlesInFeature = bundles.select {|b| find_feature_bundle(featureBundles, b[:artifactId], b[:version] != nil)}
+  bundlesInFeature.map {|b| b[:id]}  					  
+end
+
+def find_feature_bundle (featureBundles, name, version)
+  featureBundles.select {|fb| fb["Bundle-SymbolicName"] == name && fb["Bundle-Version"] == version }
+end
+
+def extract_bundle_headers (headers)
+  re = /(.+) = (.+)/
+  header =  {}
+  headers.lines.map {|l| m = l.match(re); if m then header[m[1].strip!] = m[2] end}
+  header
+end
+
+def extract_bundles_from_feature (data)
+  bundles = []
+  data.lines.each do |l|
+    m = l.match(/.*mvn:(?<GroupId>[\w\.]+)\/(?<ArtifactId>[\w-\.]+)\/(?<Version>[\d\w\.-]+)/)
+    if m then 
+      bundles.push({:groupId 	=> m['GroupId'],
+		    :artifactId	=> m['ArtifactId'],
+		    :version	=> m['Version']})
+    end
+  end
+  bundles
+end
