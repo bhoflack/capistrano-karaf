e0a531270d31977e9b435f9ecfc9edc81b603774
Brecht Hoflack <brh@melexis.com>
2015-01-21 09:09:28 +0100

    Add a sshkit backend that uses sshpass
    By proxying the communication through openssh i hope to have fewer
    issues with the karaf server.  This sshkit backend uses sshpass to send
    the command to karaf's ssh server.

--- lib/capistrano-karaf.rb
+++ lib/capistrano-karaf.rb
@@ -1,17 +1,7 @@
 require 'capistrano-karaf/core'
 require 'capistrano-karaf/extended'
 
+require_relative 'capistrano-karaf/backends/opensshproxy'
+
 include Capistrano_Karaf
-
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ lib/capistrano-karaf/backends/opensshproxy.rb
@@ -0,0 +1,50 @@
+require 'sshkit/backends/netssh'
+
+module Capistrano_karaf
+  module Backend
+    class SshProxy < SSHKit::Backend::Netssh
+
+      @pool = SSHKit::Backend::ConnectionPool.new
+
+      def capture(*args)
+          karaf_role = fetch :karaf_role, :karaf
+          if host.has_role? karaf_role
+            
+            karaf_username = fetch :karaf_username, "smx"
+            karaf_password = fetch :karaf_password, "smx"
+            karaf_port = fetch :karaf_port, "8101"
+
+            args1 = ["sshpass", "-p", karaf_password, "ssh", "-o", "NoHostAuthenticationForLocalhost=yes", "#{karaf_username}@localhost", "-p", karaf_port].concat(args)
+            options = { verbosity: Logger::DEBUG }.merge(args1.extract_options!)
+            _execute(*[*args1, options]).full_stdout.strip
+          else
+            options = { verbosity: Logger::DEBUG }.merge(args.extract_options!)
+            _execute(*[*args, options]).full_stdout.strip
+          end
+      end
+
+      def execute(*args)
+          karaf_role = fetch :karaf_role, :karaf
+          if host.has_role? karaf_role
+
+            karaf_username = fetch :karaf_username, "smx"
+            karaf_password = fetch :karaf_password, "smx"
+            karaf_port = fetch :karaf_port, "8101"
+
+            args1 = ["sshpass", "-p", karaf_password, "ssh", "-o", "NoHostAuthenticationForLocalhost=yes", "#{karaf_username}@localhost", "-p", karaf_port].concat(args)
+            _execute(*args1).success?
+          else
+            _execute(*args).success?
+          end
+      end
+
+      def has_class? (host, clazz)
+        hosts = Capistrano::DSL.roles(clazz)
+        return (not (hosts.find do |host| 
+                       puts "'#{host.hostname.class}' == #{host.hostname}"
+                       host.hostname == hostname
+                     end.nil?))
+      end
+    end
+  end
+end
